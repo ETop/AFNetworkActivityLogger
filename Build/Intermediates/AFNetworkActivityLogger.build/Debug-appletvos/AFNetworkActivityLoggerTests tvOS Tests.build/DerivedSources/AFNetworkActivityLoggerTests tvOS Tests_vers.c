c151edc62452be4f154ee77f40d9b172badaca08
钟远科 <604217454@qq.com>
2016-11-10 17:20:16 +0800

    优化 请求响应的转化逻辑

---
+++ Build/Intermediates/AFNetworkActivityLogger.build/Debug-appletvos/AFNetworkActivityLoggerTests tvOS Tests.build/DerivedSources/AFNetworkActivityLoggerTests tvOS Tests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char AFNetworkActivityLoggerTests_tvOS_TestsVersionString[];
+ extern const double AFNetworkActivityLoggerTests_tvOS_TestsVersionNumber;
+
+ const unsigned char AFNetworkActivityLoggerTests_tvOS_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:AFNetworkActivityLoggerTests tvOS Tests  PROJECT:AFNetworkActivityLogger-1" "\n";
+ const double AFNetworkActivityLoggerTests_tvOS_TestsVersionNumber __attribute__ ((used)) = (double)1.;
